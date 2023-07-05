102b80283c29998dacd178a15f3be096656a20da
raghav gupta <raghav.24@yahoo.com>
2023-07-05 09:21:07 +0530

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Grid, Paper } from '@mui/material'
+const Login = () => {
+    return (
+        <Grid>
+            <Paper elevation={10}>
+                Sign in
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Login
